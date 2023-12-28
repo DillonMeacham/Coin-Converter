759edbcd7d29a4687336b84e32c75abe7f626fd5
Dillon Meacham <meachamdillon52@gmail.com>
2023-12-28 13:54:20 -0600

    first commit

---
+++ CoinConverter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Greet the user and capture input
+            Console.WriteLine("Welcome to Money Maker!");
+
+            Console.Write("Please enter an amount to convert to coins: ");
+
+            string totalAsString = Console.ReadLine();
+
+            double total = Math.Floor(Convert.ToDouble(totalAsString));
+
+            Console.WriteLine($"{total} cents is equal to...");
+
+            // convert to coins
+
+            int gold = 10;
+            int silver = 5;
+
+            double goldCoins = Math.Floor(total / gold);
+
+            double remainder = total % gold;
+
+            double silverCoins = Math.Floor(remainder / silver);
+
+            remainder = remainder % silver;
+
+            Console.WriteLine($"Gold coins: {goldCoins}");
+
+            Console.WriteLine($"Silver coins: {silverCoins}");
+
+            Console.WriteLine($"Bronze coins: {remainder}");
+        }
+    }
+}
